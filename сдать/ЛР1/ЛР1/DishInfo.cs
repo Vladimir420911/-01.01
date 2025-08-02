466fd436a30a8923204ccd0ad5523867235135c4
Vladimir420911 <dhbtgvhb@gmail.com>
2025-08-02 16:40:45 +0300

    сделал лр1

---
+++ сдать/ЛР1/ЛР1/DishInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ЛР1
+{
+    public struct DishInfo
+    {
+        public string DishName;
+        public string DishCategory;
+        public int DayOfOrder;
+        public int Amount;
+
+        public DishInfo(string dishInfo, string dishCategory, int dayOfOrder, int amount)
+        {
+            DishName = dishInfo;
+            DishCategory = dishCategory;
+            DayOfOrder = dayOfOrder;
+            Amount = amount;
+        }
+    }
+}
---
+++ сдать/ЛР1/ЛР1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ЛР1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<DishInfo> dishes = new List<DishInfo>()
+            {
+                new DishInfo("Борщ", "суп", 1, 12),
+                new DishInfo("Щи", "суп", 2, 18),
+                new DishInfo("Гороховый суп", "суп", 2, 26),
+                new DishInfo("Сок апельсиновый", "напиток", 2, 12),
+                new DishInfo("Черный чай", "напиток", 2, 28),
+            };
+
+            Console.WriteLine("Введите название категории: ");
+            string category = Console.ReadLine();
+
+            List<DishInfo> sortedDishes = FindByCategory(dishes, category);
+            var popularDishes = sortedDishes.OrderByDescending(d => d.Amount).ToList();
+
+            foreach(var dish in popularDishes)
+            {
+                Console.WriteLine($"Наименование блюда: {dish.DishName}, кол-во заказов: {dish.Amount}");
+            }
+
+            double avgOrdersCount = sortedDishes.Average(d => d.Amount);
+            Console.WriteLine($"Среднее количество продаж по данной категории = {avgOrdersCount}");
+        }
+
+        static List<DishInfo> FindByCategory(List<DishInfo> dishInfos, string category)
+        {
+            List<DishInfo> sortedList = new List<DishInfo>();
+
+            foreach(var dish in dishInfos)
+            {
+                if(dish.DishCategory == category)
+                {
+                    sortedList.Add(dish);
+                }
+            }
+
+            return sortedList;
+        }
+
+
+    }
+}
